a0d99d960403046f7e61aeaf27b7d777b0d7ece3
Sylvia T <sylviarosemarie@gmail.com>
2023-08-08 14:28:21 -0500

    add click event re products to disply their price in a window alert upon click. made erd diagram to show relationships in data.

--- scripts/Products.js
+++ scripts/Products.js
@@ -6,10 +6,17 @@ export const Products = () => {
   let html = "<ul>";
 
   for (const product of products) {
-    html += `<li>${product.name}</li>`;
+    html += `<li data-type="product" data-price="${product.price}" data-id="${product.id}" data-name="${product.name}">${product.name}</li>`;
   }
 
   html += "</ul>";
-
   return html;
 };
+
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+  if (itemClicked.dataset.type === "product") {
+    const item = itemClicked.dataset;
+    window.alert(`${item.name} is $${item.price}`);
+  }
+});
